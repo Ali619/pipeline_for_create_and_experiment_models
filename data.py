551ce88f172613d545892304afe5d583695a21be
origin <bahadorani@partotech.com>
2022-11-21 00:53:24 +0330

    Add data_setup function to make data ready for trainig and predicting with Dataloader and Transforms

--- data.py
+++ data.py
@@ -27,3 +27,22 @@ def downlaod_data(source: str, destination: str, remove_source: bool=True) -> Pa
 
     if remove_source:
         os.remove(data_path / target_file)
+
+
+from torch.utils.data import DataLoader
+from torchvision import transforms, datasets
+
+NUM_WORKER = os.cpu_count()
+
+def data_setup(train_dir: str, test_dir: str, batch_size: int, transform: transforms.Compose, num_worker=NUM_WORKER):
+
+
+    train_data = datasets.ImageFolder(train_dir, transform=transform)
+    test_data = datasets.ImageFolder(test_dir, transform=transform)
+
+    class_names = train_data.classes
+
+    train_dataloader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True, num_workers=NUM_WORKER)
+    test_dataloader = DataLoader(dataset= test_data, batch_size=batch_size, num_workers=NUM_WORKER)   
+
+    return train_dataloader, test_dataloader, class_names
