56b01e63dfbdd95544872acd7a6c04f0edb61ce1
origin <bahadorani@partotech.com>
2022-11-21 12:57:08 +0330

    Seprate train and test data in 10 percent and 20 percent datasets

--- main.py
+++ main.py
@@ -3,29 +3,33 @@ from torchvision import transforms
 from utils import plot_random_image
 from data import downlaod_data, data_setup
 
-downlaod_data(source='https://github.com/mrdbourke/pytorch-deep-learning/raw/46a8b2988295e135e39d3320b4dcf95fe2ef3927/data/pizza_steak_sushi.zip',
+data_10_precent = downlaod_data(source='https://github.com/mrdbourke/pytorch-deep-learning/raw/46a8b2988295e135e39d3320b4dcf95fe2ef3927/data/pizza_steak_sushi.zip',
             destination='10_percent_food_pics')
 
-downlaod_data(source='https://github.com/mrdbourke/pytorch-deep-learning/raw/46a8b2988295e135e39d3320b4dcf95fe2ef3927/data/pizza_steak_sushi_20_percent.zip',
+data_20_precent = downlaod_data(source='https://github.com/mrdbourke/pytorch-deep-learning/raw/46a8b2988295e135e39d3320b4dcf95fe2ef3927/data/pizza_steak_sushi_20_percent.zip',
             destination='20_percent_food_pics')
-""
+
+train_dir_10percent = data_10_precent / 'train'
+test_dir_10percent = data_10_precent / 'test'
+
+train_dir_20percent = data_20_precent / 'train'
+test_dir_20precent = data_20_precent / 'test'
+
 weights = torchvision.models.EfficientNet_B0_Weights.DEFAULT
 model = torchvision.models.efficientnet_b0(weights=weights)
 
 auto_transform = weights.transforms()
 
-train_dir = '/home/ali/newdisk/Projects/my-project/PyTorch-tutorial/pipeline_for_create_and_experiment_models/data/10_percent_food_pics/train'
-test_dir = '/home/ali/newdisk/Projects/my-project/PyTorch-tutorial/pipeline_for_create_and_experiment_models/data/10_percent_food_pics/test'
 
-train_dataloader, test_dataloader, class_names = data_setup(train_dir=train_dir, 
-                                                            test_dir=test_dir,
+train_dataloader, test_dataloader, class_names = data_setup(train_dir=train_dir_10percent, 
+                                                            test_dir=test_dir_10percent,
                                                             batch_size=32,
                                                             transform=auto_transform)
 
 print(f"train_dataloader is: {train_dataloader}, test_dataloader is: {test_dataloader}, class_names is: {class_names}")
 
 plot_random_image(model=model,
-                    image_path=test_dir,
+                    image_path=test_dir_20precent,
                     class_names=class_names,
                     transform=auto_transform,
                     n=3)
