05e2bd5f909ac745b09e36b8d0cfcb2cececdfae
origin <bahadorani@partotech.com>
2022-11-21 16:55:15 +0330

    Create dataloader objects from train and test datasets, create models and fix their last layer to outout_size=3. Write loops and use train() function to start training and log loss and acc with object from create_writer function

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import torch
+from torch import nn
 import torchvision
 from torchvision import transforms
-# from torchinfo
-from utils import plot_random_image
+from engine import train
+from utils import plot_random_image, model_summary, create_models, create_writer, save_model
 from data import downlaod_data, data_setup
+import os
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 data_10_precent = downlaod_data(source='https://github.com/mrdbourke/pytorch-deep-learning/raw/46a8b2988295e135e39d3320b4dcf95fe2ef3927/data/pizza_steak_sushi.zip',
             destination='10_percent_food_pics')
@@ -16,21 +20,60 @@ test_dir_10percent = data_10_precent / 'test'
 train_dir_20percent = data_20_precent / 'train'
 test_dir_20percent = data_20_precent / 'test'
 
-weights = torchvision.models.EfficientNet_B0_Weights.DEFAULT
-model = torchvision.models.efficientnet_b0(weights=weights)
+BATCH_SIZE = 32
 
-auto_transform = weights.transforms()
+train_dataloader_10_percent, test_dataloader_10_percent, class_names = data_setup(train_dir=train_dir_10percent,
+                                                                                    test_dir=test_dir_10percent,
+                                                                                    batch_size=BATCH_SIZE,
+                                                                                    num_worker=os.cpu_count())
 
+train_dataloader_20_percent, test_dataloader_20_percent, class_names = data_setup(train_dir=train_dir_20percent,
+                                                                                    test_dir=test_dir_20percent,
+                                                                                    batch_size=BATCH_SIZE,
+                                                                                    num_worker=os.cpu_count())
 
-train_dataloader, test_dataloader, class_names = data_setup(train_dir=train_dir_10percent, 
-                                                            test_dir=test_dir_10percent,
-                                                            batch_size=32,
-                                                            transform=auto_transform)
+# We need to change the classifier output layer while we only have 3 class to predict (not 1000)
+# We will do this in create_models functions
+effnet_b0 = create_models(model_name='effnet_b0', num_classes=class_names)
+effnet_b2 = create_models(model_name='effnet_b2', num_classes=class_names)
 
-print(f"train_dataloader is: {train_dataloader}, test_dataloader is: {test_dataloader}, class_names is: {class_names}")
+# Creating parameters to test 2 models with diffrent hyperparameters
+num_epochs = [5, 10]
+models = [effnet_b0, effnet_b2]
+train_dataloaders = {"data_10_percent": [train_dataloader_10_percent],
+                    "data_20_percent": [train_dataloader_20_percent]}
 
-plot_random_image(model=model,
-                    image_path=test_dir_20percent,
-                    class_names=class_names,
-                    transform=auto_transform,
-                    n=3)
+# Starting loop for training 
+experiment_num = 1
+
+for train_data_name, train_data in train_dataloaders.items():
+    for model_name in models:
+        for epochs in num_epochs:
+            
+            print(f"Experiment number is: {experiment_num}",
+                    f"train_data is: {train_data_name}",
+                    f"model_name is: {model_name}",
+                    f"epoch number is: {epochs}")
+
+            # Creating loss function and optimizer
+            loss_fn = nn.CrossEntropyLoss()
+            optimizer = torch.optim.Adam(params=model_name.parameters(), lr=0.001)
+
+            result = train(model=model_name,
+                            train_dataloader=train_data,
+                            test_dataloader=test_dataloader_20_percent,
+                            loss_fn=loss_fn,
+                            optimizer=optimizer,
+                            device=device,
+                            epochs=epochs,
+                            writer=create_writer(experiment_name=train_data_name,
+                                                    model_name=model_name,
+                                                    extra=f"{epochs}_epochs"))
+
+            save_filepath = f"07_{model_name}_{train_data_name}_{epochs}_epochs.pth"
+
+            save_model(model=model_name,
+                       target_dir="models",
+                       model_name=save_filepath)
+
+            experiment_num += 1
