6dae5fa45b91dbe8cb5f1dab600c3993a583d1b3
origin <bahadorani@partotech.com>
2022-11-21 15:41:49 +0330

    Update plot_random_image function. Add document for functions

--- utils.py
+++ utils.py
@@ -8,16 +8,35 @@ from PIL import Image
 from typing import Tuple
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def plot_random_image(model: torch,
+def plot_random_image(model: torch.nn.Module,
                         image_path: str,
                         class_names: list,
                         transform: torchvision.transforms = None,
                         image_size: Tuple[int, int]=(224, 224),
-                        n: int=3,
+                        num_sample: int=3,
                         device: torch.device=device):
-    
+    """
+    This function is for predicting class and creating plot with a trained model. The samples for predicting and 
+    ploting will be choosen randomly.
+
+    Args:
+        model: the model we want to predict with.
+        image_path: the path to our test image data (data that model didn't see yet)
+        class_names: name of classes we have to be writen on plot title
+        transform: type of transform we want to do with the images before predicting. (Check this from the model documention)
+        image_size: size of image train dataset. Defualt is (224, 224)
+        num_sample: number of sample we want to predict and plot.
+        device: device to use for predicting (cpu or cuda). Defualt is set to `device` from `device = "cuda" if torch.cuda.is_available() else "cpu"`
+
+    Example: 
+            plot_random_image(model=model,
+                    image_path=test_dir_20percent,
+                    class_names=class_names,
+                    transform=auto_transform,
+                    num_sample=3)
+    """
     _image_path = list(Path(image_path).glob('*/*.jpg'))
-    random_image_path = random.sample(population=_image_path, k=n)
+    random_image_path = random.sample(population=_image_path, k=num_sample)
     
 
     for images_path in random_image_path:
@@ -53,10 +72,45 @@ def plot_random_image(model: torch,
 
 from torchinfo import summary
 def model_summary(model: torch.nn.Module, input_size: Tuple=(32, 3, 224, 224)):
+    """
+    This function will show us the model's layers and how the data shape will change in the model's layer.
 
+    Args:
+        `model`: the model we want to check the layers.
+        `input_size`: the input size as a `tuple`. Default is `(32, 3, 224, 224)` while we are using the dataset,
+         dataloader and EfficientNet models. You can change it to what you need.
+
+    """
     return summary(model=model, 
             input_size=(32, 3, 224, 224), # make sure this is "input_size", not "input_shape"
             col_names=["input_size", "output_size", "num_params", "trainable"],
             col_width=20,
             row_settings=["var_names"])
     
+from torch import nn
+def create_models(model_name: str, class_names: int) -> torch.nn.Module:
+    """
+    This function will craete a model which is only `effnet_b0` for EfficientNet_b0 or `effnet_b2` for EfficientNet_b2.
+    While Efficient models has 1000 nodes in the linear layer (last layer / classify layer), we need to reduce it to the 
+    number of our custom datasets classes. 
+
+    Args:
+        `model_name`: the model's namm we want to create.
+        `class_names`: the amount of classes that our data has (number of classes we want to predict)
+
+
+    Example:
+        create_models(model_name=`effnet_b0`, class_names= `3`)
+    """
+    if model_name == 'effnet_b0':
+        weights = torchvision.models.EfficientNet_B0_Weights.DEFAULT
+        model = torchvision.models.efficientnet_b0(weights=weights).to(device)
+        model.classifier = nn.Sequential(nn.Dropout(p=0.2), nn.Linear(in_features=1280, out_features=class_names))
+
+
+    elif model_name == "effnet_b2":
+        weights = torchvision.models.EfficientNet_B2_Weights.DEFAULT
+        model = torchvision.models.efficientnet_b2(weights=weights).to(device)
+        model.classifier = nn.Sequential(nn.Dropout(p=0.3), nn.Linear(in_features=1408, out_features=class_names))
+
+    return model
